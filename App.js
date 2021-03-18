678abac84ef978ec1046a0dfac755ea80baf97b8
Sakhawat Hossain <hossain.sakhawat93@gmail.com>
2021-03-18 15:42:38 -0400

    small changes
    few changes

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import {TouchableOpacity, KeyboardAvoidingView, StyleSheet, Text, View, TextInput } from 'react-native';
+import Task from './components/Task';
+
 
 export default function App() {
+  const [task, setTask] = useState();
+
+  const handleAddTask = () => {
+    console.log(task);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    {/* Reminders */}
+    <View style={styles.tasksWrapper}>
+      <Text style={styles.sectionTitle}>Bills Payment Reminder</Text>
+      <View style={styles.items}>
+      {/* list starts from here  */}
+        <Task text={'CC Payment 1'} />
+        <Task text={'CC Payment 2'} />
+      </View>
+    </View>
+     
+
+     {/* where they input the reminders */}
+     <KeyboardAvoidingView 
+     behavior={Platform.OS === "ios" ? "padding" : "height"}
+     style={styles.writeTaskWrapper}>
+
+     <TextInput style={styles.input} placeholder={'What bill do you need to pay?'} value={task} onChangeText={text => setTask(text)}/>
+     <TouchableOpacity onPress={() => handleAddTask() }>
+       <View style={styles.addWrapper}>
+        <Text style={styles.addText}>+</Text>
+       </View>
+
+     </TouchableOpacity>
+     </KeyboardAvoidingView>
     </View>
   );
 }
@@ -14,8 +45,57 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'grey',
+   
   },
+
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+sectionTitle: {
+  fontSize: 24,
+  fontWeight: 'bold',
+  // color: 'white',
+  
+},
+items: {
+  marginTop: 30,
+},
+  // white: {
+  //   color: 'white',
+  // },
+
+
+
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+input: {
+paddingVertical: 15,
+paddingHorizontal: 15,
+backgroundColor: '#fff',
+borderRadius: 60,
+borderColor: '#C0C0C0',
+borderWidth: 1,
+width: 250,
+},
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
+
+
 });
